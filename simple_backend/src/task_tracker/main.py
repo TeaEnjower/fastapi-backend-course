d155772cdd63b12e4e56f3a69d1a81c6ecb87631
George Kucherov <ku4erov.g@mail.ru>
2025-07-10 15:22:30 +0300

    реализованно хранение данных в JSON bin

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,19 +1,72 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import requests
 
 app = FastAPI()
 
-@app.get("/tasks")
-def get_tasks():
-    pass
+class Task(BaseModel):
+    text: str
+    is_done: bool = False  
 
-@app.post("/tasks")
-def create_task(task):
-    pass
+# Конфигурация JSONBin.io
+headers = {
+    'Content-Type': 'application/json',
+    'X-Master-Key': '$2a$10$92anMMSpReZf03/15uRLVeWseU19ToTVWdu5qHUVY5p0M.uc9BNNq',  
+    'X-Access-Key': '$2a$10$bag0phEtSGHeSS8uovM9MOU3bRW.7piFdMY5TEEEGeHlq1GSoA4P6',
+}
 
-@app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    pass
+BIN_ID = '686d58048960c979a5b952d9'
+URL = f'https://api.jsonbin.io/v3/b/{BIN_ID}'
+
+# Инициализация хранилища
+try:
+    response = requests.get(f"{URL}/latest", headers=headers)
+    tasks = [Task(**item) for item in response.json()['record']]
+except:
+    tasks = []
+
+
+@app.on_event("startup")
+def verify_connection():
+    try:
+        response = requests.get(
+            f"https://api.jsonbin.io/v3/b/{BIN_ID}",
+            headers={'X-Master-Key': '$2a$10$92anMMSpReZf03/15uRLVeWseU19ToTVWdu5qHUVY5p0M.uc9BNNq'}
+        )
+        print('Подключение к JSONBin прошло успешно')
+        assert response.status_code == 200
+    except Exception as e:
+        print("❌ Ошибка подключения к JSONBin:", str(e))
+        raise
+
+@app.get("/tasks", response_model=list[Task])
+def list_tasks(limit: int = 6):
+    return tasks[:limit]
+
+@app.post("/tasks", response_model=list[Task])
+def create_task(task: Task):
+    tasks.append(task)
+    response = requests.put(URL, json=[t.dict() for t in tasks], headers=headers)
+    if response.status_code != 200:
+        tasks.pop()  # Откат при ошибке
+        raise HTTPException(500, detail=f"JSONBin error: {response.text}")
+    return tasks
+
+@app.get("/tasks/{task_id}", response_model=Task)
+def get_task(task_id: int):
+    try:
+        return tasks[task_id]
+    except IndexError:
+        raise HTTPException(404, detail='Task not found')
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    pass
+    try:
+        deleted = tasks.pop(task_id)
+        response = requests.put(URL, json=[t.dict() for t in tasks], headers=headers)
+        if response.status_code != 200:
+            tasks.insert(task_id, deleted)  # Откат при ошибке
+            raise HTTPException(500, detail="Failed to update cloud storage")
+        return {"status": "deleted"}
+    except IndexError:
+        raise HTTPException(404, detail='Task not found')
